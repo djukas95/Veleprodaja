c9a554d3efd9311606ffe5e6bae067d92070ea6c
Ognjen Djukic <djukara95@hotmail.com>
2018-12-01 18:33:25 +0100

    LoginForm

--- Veleprodaja/LoginForm.Designer.cs
+++ Veleprodaja/LoginForm.Designer.cs
@@ -115,7 +115,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(289, 52);
+            this.label1.Location = new System.Drawing.Point(289, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 21);
             this.label1.TabIndex = 6;
